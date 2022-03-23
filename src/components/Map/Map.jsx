a5aa5c1bb768ffebbc716b9743c50e1fbbd33199
oliver <olivercastroteran@gmail.com>
2022-03-23 15:54:56 -0400

    app styles added with MUI

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,21 @@
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
+import Rating from '@material-ui/lab';
+import useStyles from './styles';
+
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+
+  const coordinates = { lat: 0, lng: 0 };
+
   return (
-    <div className="map">
-      <h2>Map</h2>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: '' }}
+        defaultCenter={coordinates}
+      ></GoogleMapReact>
     </div>
   );
 };
