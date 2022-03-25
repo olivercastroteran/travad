6798d1cc1b29c1ad4a8c9cd46b7509d0a7e5c9e5
oliver <olivercastroteran@gmail.com>
2022-03-24 22:36:25 -0400

    rendering map - google maps api

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,6 @@
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab';
 import useStyles from './styles';
 
@@ -15,6 +15,12 @@ const Map = () => {
       <GoogleMapReact
         bootstrapURLKeys={{ key: '' }}
         defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
       ></GoogleMapReact>
     </div>
   );
